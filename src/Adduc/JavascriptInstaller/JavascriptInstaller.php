fce5de5c54c1478d7774548bf7015852ec7f3058
John Long <a@88k.us>
2012-12-28 08:15:53 -0600

    Test 8

---
+++ src/Adduc/JavascriptInstaller/JavascriptInstaller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Adduc\JavascriptInstaller;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class JavascriptInstaller extends LibraryInstaller {
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package) {
+        var_dump($package);
+        throw new \Exception("asdf");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType) {
+        return 'javascript-library' === $packageType;
+    }
+
+}
