53650b6fab7b7c5748a6ff5e32bef2ce07447dfc
John Long <a@88k.us>
2012-12-28 08:49:47 -0600

    Test 42.1

--- src/Adduc/JavascriptInstaller/JavascriptInstaller.php
+++ src/Adduc/JavascriptInstaller/JavascriptInstaller.php
@@ -7,17 +7,28 @@ use Composer\Installer\LibraryInstaller;
 
 class JavascriptInstaller extends LibraryInstaller {
 
-    /**
-     * {@inheritDoc}
-     */
-    public function getInstallPath(PackageInterface $package) {
-        $extra = $package->getExtra();
+    public function __construct(IOInterface $io, Composer $composer, $type = 'library') {
+        parent::__construct($io, $composer, $type);
+
+        $extra = $this->composer->package->getExtra();
         if(isset($extra['javascript-dir'])) {
-            return $extra['javascript-dir'];
+            $this->javascriptDir = $extra['javascript-dir'];
         } else {
 	    var_dump($this->composer);
             throw new \InvalidArgumentException("javascript-dir must be defined in extra");
         }
+        
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package) {
+        $this->initializeJavascriptDir();
+        $targetDir = $package->getTargetDir();
+
+        return ($this->javascriptDir ? $this->javascriptDir.'/' : '') . $package->getPrettyName() . ($targetDir ? '/'.$targetDir : '');
     }
 
     /**
@@ -25,6 +36,11 @@ class JavascriptInstaller extends LibraryInstaller {
      */
     public function supports($packageType) {
         return 'javascript-library' === $packageType;
+    }
+
+    public function initializeJavascriptDir() {
+        $this->filesystem->ensureDirectoryExists($this->javascriptDir);
+        $this->javascriptDir = realpath($this->javascriptDir);        
     }
 
 }
